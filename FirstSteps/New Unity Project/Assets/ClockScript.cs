01cb3e7a0b5420b0277750aae078d8c8e33bb669
Ted Wu <tedwu2016@u.northwestern.edu>
2016-01-09 13:28:55 -0600

    clock scripts

---
+++ FirstSteps/New Unity Project/Assets/ClockScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.log(string.Format("Start called at {0} seconds", Time.time));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.log(string.Format("Update called at {0} seconds", Time.time));
+	}
+}
