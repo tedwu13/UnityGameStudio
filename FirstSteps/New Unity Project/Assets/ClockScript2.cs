0631adf8eff4be588fcf6eaa5a4bfc484af5b1e2
Ted Wu <tedwu2016@u.northwestern.edu>
2016-01-10 16:47:20 -0600

    finalize clock project

--- FirstSteps/New Unity Project/Assets/ClockScript2.cs
+++ FirstSteps/New Unity Project/Assets/ClockScript2.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class ClockScript2 : MonoBehaviour {
 
+	public GameObject _secondHand, _minuteHand, _hourHand;
 	// Use this for initialization
 	void Start () {
-		Debug.log(string.Format("Start called at {0} seconds", Time.time));
+		_hourHand = GameObject.Find("HourHand");
+		_minuteHand = GameObject.Find("MinuteHand");
+		_secondHand = GameObject.Find("SecondHand");
+//		Debug.Log(string.Format("Start called at {0} seconds", Time.time));
+	}
+
+	private void RotateByFraction(GameObject obj, float fraction) {
+		Transform transform = obj.GetComponent<Transform>();
+		transform.localRotation = Quaternion.Euler (0, 0, fraction * 360);
 	}
 
 	// Update is called once per frame
 	void Update () {
-		Debug.log(string.Format("Update called at {0} seconds", Time.time));
+		//convert current second to an angle then transform by setting rotation vector
+		RotateByFraction (_hourHand, DateTime.Now.Hour /12f);
+		RotateByFraction (_minuteHand, DateTime.Now.Minute / 60f);
+		RotateByFraction (_secondHand, DateTime.Now.Second / 60f);
+		//		Debug.Log(string.Format("Update called at {0} seconds", Time.time));
 	}
 }
---
+++ FirstSteps/New Unity Project/Assets/TimeDisplayScript.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeDisplayScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		string currentTime = DateTime.Now.ToShortTimeString ();
+		this.GetComponent<Text> ().text = currentTime;
+		//or this.gameObject.getCompnent();
+	}
+}
