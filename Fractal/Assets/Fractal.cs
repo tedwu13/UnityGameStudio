c689b631512eba2d4d4ca7c72e56154b6da90a72
Ted Wu <tedwu2016@u.northwestern.edu>
2016-01-10 23:30:23 -0600

    fractal project tutorial

---
+++ Fractal/Assets/Fractal.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+
+	public Mesh mesh;
+	public Material material;
+	public float childScale;
+	private void Start () {
+		gameObject.AddComponent<MeshFilter>().mesh = mesh;
+		gameObject.AddComponent<MeshRenderer>().material = material;
+		if (depth < maxDepth) {
+			StartCoroutine(CreateChildren());
+		}
+	}
+
+	private IEnumerator CreateChildren () {
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child").AddComponent<Fractal>().
+		Initialize(this, Vector3.up, Quaternion.identity);
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child").AddComponent<Fractal>().
+		Initialize(this, Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+		yield return new WaitForSeconds(0.5f);
+		new GameObject("Fractal Child").AddComponent<Fractal>().
+		Initialize(this, Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+	}
+
+	private void Initialize (Fractal parent, Vector3 direction) {
+		mesh = parent.mesh;
+		material = parent.material;
+		maxDepth = parent.maxDepth;
+		depth = parent.depth + 1;
+		childScale = parent.childScale;
+		transform.parent = parent.transform;
+		transform.localScale = Vector3.one * childScale;
+		transform.localPosition = direction * (0.5f + 0.5f * childScale);
+	}
+}
