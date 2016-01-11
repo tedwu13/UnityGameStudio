ffeae8730fc783fae90da3fd36d83dbced47d46b
Ted Wu <tedwu2016@u.northwestern.edu>
2016-01-10 18:09:09 -0600

    runner platform game

---
+++ Runner/Assets/RunnerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RunnerScript : MonoBehaviour {
+	public Vector3 Acceleration;
+	private bool _IsTouchingPlatform;
+	void Update () {
+	}
+
+	void FixedUpdate() {
+		if (_IsTouchingPlatform) {
+			var rigidbody = this.GetComponent<Rigidbody> ();
+			rigidbody.AddForce (Acceleration, ForceMode.Acceleration);
+		}
+
+	}
+
+	void OnCollisionEnter () {
+		_IsTouchingPlatform = true;
+	}
+
+	void OnCollisonExit() {
+		_IsTouchingPlatform = false; 
+	}
+
+}
